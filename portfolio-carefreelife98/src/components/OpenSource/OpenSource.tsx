a972a58f435a41cf2ad2fd95ed8e0006bf022445
CarefreeLife <csm4903@naver.com>
2024-07-30 17:57:23 +0900

    Portfolio 1차 작성

--- portfolio-carefreelife98/src/components/OpenSource/OpenSource.tsx
+++ portfolio-carefreelife98/src/components/OpenSource/OpenSource.tsx
@@ -1,45 +1,81 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import ApolloClient, { gql } from 'apollo-boost';
 import './OpenSource.css';
 import GithubCard from '../GithubCard/GithubCard';
 import { openSourceProjects } from '../../portfolio';
 
 function OpenSource() {
-    const MAX_REPO_NUM = 6;
-    const [repos, setRepos] = useState([]);
+    const [ repos, setRepos ] = useState([]);
 
     useEffect(() => {
-        fetchRepoData();  // eslint-disable-next-line react-hooks/exhaustive-deps
+        getRepoData();
     }, []);
 
-    const fetchRepoData = async () => {
-        try {
-            console.log('gh token: ' + process.env.REACT_APP_GITHUB_TOKEN)
-            const response = await axios.get('https://api.github.com/users/carefreelife98/repos', {
-                headers: {
-                    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
-                },
+    function getRepoData(): void {
+        // I don't know well about this part...
+        const client = new ApolloClient({
+            uri: "https://api.github.com/graphql",
+            request: (operation) => {
+                operation.setContext({
+                    headers: {
+                        authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+                    },
+                });
+            },
+        });
+
+        client
+            .query({
+                query: gql`
+                {
+                user(login: "carefreelife98") {
+                    pinnedItems(first: 6, types: [REPOSITORY]) {
+                        totalCount
+                        edges {
+                            node {
+                                ... on Repository {
+                                    name
+                                    description
+                                    forkCount
+                                    stargazers {
+                                        totalCount
+                                    }
+                                    url
+                                    id
+                                    diskUsage
+                                    primaryLanguage {
+                                        name
+                                        color
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            `,
+            })
+            .then((result) => {
+                setrepoFunction(result.data.user.pinnedItems.edges);
+            })
+            .catch(function (error) {
+                console.log(error);
             });
+    }
 
-            // 가져온 데이터 중 최대 6개 레포만 저장.
-            const limitedRepos = response.data.slice(0, MAX_REPO_NUM);
-            setRepos(limitedRepos);
-        } catch (error) {
-            console.error("Error fetching repositories", error);
-        }
-    };
-
+    function setrepoFunction(arr: []): void {
+        setRepos(arr);
+    }
     return (
         <div className="main" id="opensource">
             <h1 className="project-title">{openSourceProjects.title}</h1>
             <div className="repo-cards">
-                {repos.map((repo, i) => (
-                    <GithubCard repos={repo} key={i} />
-                ))}
+                {repos.map((repo, i) => {
+                    return <GithubCard repos={repo} key={i} />;
+                })}
             </div>
-            <a href="https://github.com/carefreelife98" target="_blank" rel="noopener noreferrer">More Projects</a>
+            <a href="https://github.com/Ikalli" target="_blank" rel="noopener noreferrer">More Projects</a>
         </div>
     );
 }
-
 export default OpenSource;
