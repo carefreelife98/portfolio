ef135b187f8a51dbd09b681712d57ba9f973470f
CarefreeLife <csm4903@naver.com>
2024-07-30 16:25:25 +0900

    Portfolio 1차 작성

--- portfolio-carefreelife98/src/portfolio.tsx
+++ portfolio-carefreelife98/src/portfolio.tsx
@@ -39,8 +39,8 @@ export const skills = {
 	// Go to https://fontawesome.com/ and search icon
 	lists: [
 		{fontAwesome: "fab fa-java", text: "Java / Spring(+Boot)", proficiency: 80},
-		{fontAwesome: "fab fa-database", text: "MySQL", proficiency: 70},
-		{fontAwesome: "fab fa-file-code", text: "Couchbase, Redis", proficiency: 60},
+		{fontAwesome: "fas fa-database", text: "MySQL", proficiency: 70},
+		{fontAwesome: "far fa-file-code", text: "Couchbase, Redis", proficiency: 60},
 		{fontAwesome: "fab fa-react", text: "React", proficiency: 40},
 		{fontAwesome: "fab fa-aws", text: "AWS", proficiency: 30},
 		{fontAwesome: "fab fa-jira", text: "Jira", proficiency: 70},
