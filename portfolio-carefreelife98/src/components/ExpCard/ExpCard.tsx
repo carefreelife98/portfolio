1f3bad769e586971444e40f047537a47a454a02d
CarefreeLife <csm4903@naver.com>
2024-07-31 17:34:57 +0900

    Portfolio: Experience Image 삽입 기능 추가

--- portfolio-carefreelife98/src/components/ExpCard/ExpCard.tsx
+++ portfolio-carefreelife98/src/components/ExpCard/ExpCard.tsx
@@ -8,7 +8,7 @@ function ExpCard({ experience }: Props) {
         <div className="expcard-main">
             <div className="inner">
 
-                <img className="expcard-company-image" src={require(`../../assets/image/company/${experience.image}`).default} alt='company-image'/>
+                <img className="expcard-company-image" src={`${process.env.PUBLIC_URL}/assets/image/company/${experience.image}`} alt='company-image'/>
                 <div className="expcard-company">
                     {experience.company}
                 </div>
