1d6c1b77830b4e1cf1a04cf7eb3a2637e7cd1e99
CarefreeLife <csm4903@naver.com>
2024-07-31 20:31:43 +0900

    Portfolio: Hello wrapper justify-content: space-around 추가

--- portfolio-carefreelife98/src/components/Hello/Hello.tsx
+++ portfolio-carefreelife98/src/components/Hello/Hello.tsx
@@ -2,44 +2,40 @@ import React from 'react';
 import './Hello.css';
 import { greeting } from '../../portfolio';
 import Social from '../Social/Social';
+import { Fade } from "react-reveal";
 
 function Hello() {
-	const Fade: any = require("react-reveal/Fade");
 	return (
-		<Fade bottom duration={800}>
 		<section className="section--hello">
 			<div className="inner">
-
-				<div className="wrapper">
-					<div className="text-title">
-						{greeting.title}&nbsp;
-						<span className="hello-emoji">👋</span>
+				<Fade bottom duration={800}>
+					<div className="wrapper">
+						<div className="text-title">
+							{greeting.title}&nbsp;
+							<span className="hello-emoji">👋</span>
+						</div>
+						<div className="role">
+							<img className="hello-image"
+								 src={`${process.env.PUBLIC_URL}/assets/image/profile/${greeting.image}`}
+								 alt='hello-image'/>&nbsp;
+						</div>
+						<div className="role">
+							<span>{greeting.role}</span>
+						</div>
+						<div className="text-subtitle">
+							<span>{greeting.subTitle}</span>
+						</div>
+						<div className="social-links">
+							<Social/>
+						</div>
+						<div className="wrapper-links">
+							<a href="#contact" id="about">Contact &gt;</a>
+							<a href={greeting.resumeLink} id="resume" target="_blank" rel="noopener noreferrer">View Resume &gt;</a>
+						</div>
 					</div>
-					<div className="role">
-						<img className="hello-image"
-							 src={`${process.env.PUBLIC_URL}/assets/image/profile/${greeting.image}`}
-							 alt='hello-image'/>&nbsp;
-						{/*<span className="hello-emoji">👋</span>*/}
-					</div>
-					<div className="role">
-						<span>{greeting.role}</span>
-					</div>
-					<div className="text-subtitle">
-						<span>{greeting.subTitle}</span>
-					</div>
-					<div className="social-links">
-						<Social/>
-					</div>
-					<div className="wrapper-links">
-						<a href="#contact" id="about">Contact &gt;</a>
-						<a href={greeting.resumeLink} id="resume" target="_blank" rel="noopener noreferrer">View
-							Resume &gt;</a>
-					</div>
-				</div>
-
+				</Fade>
 			</div>
 		</section>
-		</Fade>
 	);
 }
 
